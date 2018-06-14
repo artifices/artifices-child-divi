ab6c88bae29bb96d0a931dc582b1de48adb4b391
Artifices.net <info@artifices.net>
2018-06-14 11:22:11 +0200

    Creación de repositorio

---
+++ divi-child-theme-artifices-generico/functions.php
@@ -0,0 +1,13 @@
+<?php
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+
+/* TUS FUNCIONES PERSONALIZADAS A PARTIR DE AQUI____ */
+
+/* Las paginas privadas son visibles por subscriptores */
+
+$subRole = get_role( 'subscriber' );
+$subRole->add_cap( 'read_private_pages' );
