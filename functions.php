6a7bbe9402fdec3b13b709f8252a3c3a27615e39
Artifices.net <info@artifices.net>
2019-04-04 12:50:03 +0200

    Eliminada función de optimización de contact form 7

--- functions.php
+++ functions.php
@@ -14,20 +14,3 @@ $subRole->add_cap( 'read_private_pages' );
 
 /* TGM Plugin activation */
 require_once get_stylesheet_directory() . '/inc/tgm_pa.php';
-
-/* Cargar Contact Form 7 allí dónde encuentre su SHORTCODE */
-if (!is_admin() && WPCF7_LOAD_JS) {
-if (!function_exists('jclconsultor_wpcf7_scripts')){
-function jclconsultor__wpcf7_scripts($content) {
-$pos=strpos($content,'class="wpcf7"');
-if ($pos===false){
-if (WPCF7_LOAD_JS){
-wp_deregister_script('contact-form-7');
-wp_deregister_style('contact-form-7');
-}
-}
-return $content;
-}
-add_filter('the_content','jclconsultor_wpcf7_scripts',99999);
-}
-}
